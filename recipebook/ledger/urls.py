a6aa18b146e0a4463543e69560ab584b4009a717
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-05 22:55:49 +0800

    housekeeping

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,6 +1,8 @@
+
 from django.urls import path
 from . import views
 
+# Reduce to two before submission
 urlpatterns = [
     path('', views.index, name="index"),
     path('recipes/list/', views.index, name="index"), 
