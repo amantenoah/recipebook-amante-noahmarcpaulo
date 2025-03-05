df285c05df441cb40ee3d0b358b6f224177f6950
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-06 00:10:38 +0800

    added basic admin page, halfway done with views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 # Reduce to two before submission
 urlpatterns = [
-    path('', views.index, name="index"),
     path('recipes/list/', views.index, name="index"), 
     path('recipe/<int:num>', views.recipe, name="recipe"),
 ]
