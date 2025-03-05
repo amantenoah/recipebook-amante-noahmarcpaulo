efa44b0623c8a1c5dbd1a349cd0e7ad0be21b7bd
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-05 23:30:22 +0800

    first draft on models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.id)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('index', args=[str(self.id)])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.PositiveIntegerField(null=False)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient_details')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredient')
