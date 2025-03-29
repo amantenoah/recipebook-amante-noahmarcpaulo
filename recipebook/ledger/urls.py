eda8bff9016851c82032cc1f0aa35837b6151d89
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-29 22:56:47 +0800

    fixed buggy urls, bootstrap settings are harmful will remove some soon

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('recipes/list/', views.index, name="index"), 
     path('recipe/<int:num>', views.recipe, name="recipe"),
+    path('recipe/add', views.new_recipe, name="add_recipe"),
+    path('recipe/<int:pk>/add_image', views.add_image, name="add_image")
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import Recipe, RecipeIngredient, RecipeImage
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+# Renders an index of all available recipes
 def index(request):
-    # Just gets all recipes since everything needed can be accessed from there
     recipes = Recipe.objects.all()
     ctx = { 
            "recipes": recipes
         }
     return render(request, "index.html", ctx)
 
+# Shows a specific recipe detailed based on the primary key passed
 @login_required
 def recipe(request, num=1):
     recipe = Recipe.objects.get(pk=num)
@@ -24,3 +26,13 @@ def recipe(request, num=1):
         "images": images
     }
     return render(request, "recipe.html", ctx)
+
+# Renders form for creating a new recipe and/or ingredients as necessary
+@login_required
+def new_recipe(request):
+    return HttpResponse("hehe")
+
+# Allows adding of images to a specific recipe page
+@login_required
+def add_image(request, pk=1):
+    return "oopsies"
